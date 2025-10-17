e988ff535f9bf5e43e90d2e3d1f2e89625cca3d4
Maksym Martynenko <maksym_martynenko@epam.com>
2025-10-17 13:25:12 +0000

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-import { mount } from "marketing/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-console.log(mount);
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <hr />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <hr />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
