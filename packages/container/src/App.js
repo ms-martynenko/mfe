030e0f34dfa644478dc839695c48d566c4e761ed
Maksym Martynenko <maksym_martynenko@epam.com>
2025-10-17 07:15:58 +0000

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 import { mount } from "marketing/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
 
 console.log(mount);
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <hr />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
